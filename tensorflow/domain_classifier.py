0c7f0a2869857d7281014e120ebb98cddc83b7f3
fin10 <fender64@gmail.com>
2016-11-20 22:46:40 +0900

    implement IOB representation

--- tensorflow/domain_classifier.py
+++ tensorflow/domain_classifier.py
@@ -2,11 +2,11 @@ import argparse
 
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
-group.add_argument('--train', metavar=('CONFIG'), help='input a config file for train')
-group.add_argument('--test', metavar=('CONFIG'), help='input a config file for test')
+group.add_argument('--train', metavar='CONFIG', help='input a config file for train')
+group.add_argument('--test', metavar='CONFIG', help='input a config file for test')
 args = parser.parse_args()
 
-if args.train == None and args.test == None:
+if args.train is None and args.test is None:
     parser.print_help()
     exit()
 
@@ -15,13 +15,14 @@ import numpy as np
 import random
 import json
 
+
 def parse_config(file_path):
     with open(file_path, 'r') as f:
         config = json.loads(f.read())
     return config['word_embedding'], config['training_data']
 
+
 def init_word_embedding(path):
-    voca = dict()
     dimension = -1
     with open(path, 'r') as f:
         voca = eval(f.read())
@@ -32,23 +33,28 @@ def init_word_embedding(path):
 
     return voca, dimension
 
-def init_training_data(input):
+
+def init_training_data(data):
     domains = []
     train_data = []
     test_data = []
-    for domain in input:
+    for domain in data:
         domains.append(domain['domain'])
         with open(domain['train'], 'r') as train_file:
-            train = [[sentence.strip().lower(), domains.index(domain['domain'])] for sentence in train_file]
+            train = [[sentence['uttr'].strip().lower(), domains.index(domain['domain'])] for sentence in
+                     json.load(train_file)]
         with open(domain['test'], 'r') as test_file:
-            test = [[sentence.strip().lower(), domains.index(domain['domain'])] for sentence in test_file]
+            test = [[sentence['uttr'].strip().lower(), domains.index(domain['domain'])] for sentence in
+                    json.load(test_file)]
+        print('[%s] train: %d, test: %d' % (domain['domain'], len(train), len(test)))
         print('[%s] train: %d, test: %d' % (domain['domain'], len(train), len(test)))
         train_data += train
         test_data += test
     random.shuffle(train_data)
     random.shuffle(test_data)
-    
+
     return domains, train_data, test_data
+
 
 word_embedding_info, domain_infos = parse_config(args.train)
 print('-- Config --')
@@ -69,6 +75,7 @@ test_count = len(test_data)
 UNK = np.zeros([embedding_dimension], dtype=np.float32)
 UNK.fill(-1)
 
+
 def init_batch(data, batch_size, label_count):
     batch = []
     labels = np.zeros([batch_size, label_count], dtype=np.int)
@@ -76,12 +83,15 @@ def init_batch(data, batch_size, label_count):
     for i in range(batch_size):
         sentence = []
         for word in data[i][0].split():
-            if word in voca :    sentence.append(voca[word])
-            else :               sentence.append(UNK)
+            if word in voca:
+                sentence.append(voca[word])
+            else:
+                sentence.append(UNK)
         batch.append(sentence)
         labels[i][data[i][1]] = 1
-        
+
     return batch, labels
+
 
 def find_longest_length(items):
     longest_length = 0
@@ -89,12 +99,14 @@ def find_longest_length(items):
         length = len(item)
         if longest_length < length:
             longest_length = length
-            
+
     return longest_length
+
 
 def extend(items, size):
     for item in items:
         item.extend([np.zeros(embedding_dimension, dtype=np.float32) for _ in range(size - len(item))])
+
 
 batch_for_train, labels_for_train = init_batch(train_data, BATCH_SIZE, label_count)
 batch_for_test, labels_for_test = init_batch(test_data, test_count, label_count)
@@ -102,10 +114,12 @@ longest_length = find_longest_length(batch_for_train + batch_for_test)
 print('longest length:%d' % longest_length)
 
 extend(batch_for_train, longest_length)
-extend(batch_for_test, longest_length)        
+extend(batch_for_test, longest_length)
+
 
 def init_weights(shape):
     return tf.Variable(tf.random_normal(shape, stddev=0.01))
+
 
 def model(X, W, B, lstm_size, step_size):
     X_split = tf.split(0, step_size, X)
@@ -114,6 +128,7 @@ def model(X, W, B, lstm_size, step_size):
     outputs, _states = tf.nn.rnn(lstm, X_split, dtype=tf.float32)
 
     return tf.matmul(outputs[-1], W) + B, lstm.state_size
+
 
 X = tf.placeholder("float", [None, embedding_dimension])
 Y = tf.placeholder("float", [label_count])
@@ -136,29 +151,28 @@ with tf.Session() as sess:
 
     failed_list = []
     for epoch in range(EPOCH_SIZE):
-        print('epoch #%d' % (epoch+1))
+        print('epoch #%d' % (epoch + 1))
         print('training...')
         for i in range(len(batch_for_train)):
-            sess.run(train_op, feed_dict={ X: batch_for_train[i], Y: labels_for_train[i] })
+            sess.run(train_op, feed_dict={X: batch_for_train[i], Y: labels_for_train[i]})
             if (i % 1000) == 999:
-                print('  loop %d' % (i+1))
+                print('  loop %d' % (i + 1))
 
-        model_path = saver.save(sess, './domain_classifier_model.ckpt', global_step=(epoch+1))
+        model_path = saver.save(sess, './domain_classifier_model.ckpt', global_step=(epoch + 1))
         print('  model saved in file: %s' % model_path)
 
         print('testing...')
         failed_list.clear()
-        failed_list.append('epoch #%d' % (epoch+1))
+        failed_list.append('epoch #%d' % (epoch + 1))
         count = 0
         for i in range(len(batch_for_test)):
-            result = sess.run(predict_op, feed_dict={ X: batch_for_test[i] })
+            result = sess.run(predict_op, feed_dict={X: batch_for_test[i]})
             correct = np.argmax(labels_for_test[i], axis=0)
             if result == correct:
                 count += 1
             else:
                 failed_list.append('matching_failed: %s %s' % (test_data[i], result))
-        print('  score: %s (%d/%d)' % ('{:.2%}'.format(count/len(batch_for_test)), count, len(batch_for_test)))
+        print('  score: %s (%d/%d)' % ('{:.2%}'.format(count / len(batch_for_test)), count, len(batch_for_test)))
 
     with open('matching_failed.out', 'w') as f:
         f.write('\n'.join(failed_list))
-
--- tensorflow/train/generate_data.py
+++ tensorflow/train/generate_data.py
@@ -1,34 +1,7 @@
 import argparse
-import re
+import json
 import random
-
-alternatePattern = re.compile('\([ <>a-zA-Z0-9\|]+\)')
-replacePattern = re.compile('<[-_a-zA-Z0-9]+>')
-
-def alternate(text, words, output):
-    try:
-        word = words.pop(0)
-        ws = word[1:-1].split('|')
-        for w in ws:
-            alternate(text.replace(word, w, 1), words[:], output)
-    except IndexError as e:
-        output.append(text)
-        return
-        
-def replace(text, slots, output):
-    try:
-        slot = slots.pop(0)
-        slotFile = open('./' + slot[1:-1], 'r')
-        for word in slotFile:
-            word = word.strip();
-            if (len(word) > 0):
-                replace(text.replace(slot, word, 1), slots[:], output)
-    except IndexError as e:
-        output.append(text)
-        return
-    except OSError as e:
-        print(e)
-        return
+import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input', help='Input a grammar file')
@@ -36,25 +9,94 @@ parser.add_argument('num_train', type=int, help='Number of training data')
 parser.add_argument('num_test', type=int, help='Number of test data')
 args = parser.parse_args()
 
+
+def alternate(text, words, output):
+    try:
+        word = words.pop(0)
+        ws = word[1].split('|')
+        for w in ws:
+            alternate(text.replace(word[0], w, 1), words[:], output)
+    except IndexError:
+        output.append(text)
+        return
+
+
+def replace(text, entities, output):
+    try:
+        entity = entities.pop(0)
+        slot_file = open('./' + entity[1], 'r')
+        for word in slot_file:
+            word = word.strip().lower()
+            if len(word) > 0:
+                replace(text.replace(entity[0], word, 1), entities[:], output)
+    except IndexError:
+        output.append(text)
+        return
+    except OSError as e:
+        print(e)
+        return
+
+
+def extract_iob(input):
+    uttr = []
+    iob = []
+    pattern = re.compile('(\S+)/([bB|iI-]\S+)')
+    for word in input.split(' '):
+        found = re.findall(pattern, word)
+        if not found:
+            uttr.append(word)
+            iob.append('o')
+        else:
+            uttr.append(found[0][0])
+            iob.append(found[0][1])
+
+    return ' '.join(uttr), ' '.join(iob)
+
+
 with open(args.input, 'r') as input:
+    replacePattern = re.compile('(<(\S+)>)')
+    alternatePattern = re.compile('(\(([^)]+)\))')
+    slotPattern = re.compile('(\{([^\}]+)\}\[(\S+)\])')
+
     result = []
-    result2 = []
     for line in input:
         utterance = line.strip().lower()
         words = re.findall(alternatePattern, utterance)
         alternate(utterance, words, result)
-        for item in result:
-            slots = re.findall(replacePattern, item)
-            replace(item, slots, result2)
-        
+
+    result2 = []
+    for item in result:
+        entities = re.findall(replacePattern, item)
+        replace(item, entities, result2)
+
     random.shuffle(result2)
-    train = result2[:args.num_train]
-    test = result2[args.num_train:(args.num_train + args.num_test)]
-    
-    with open((args.input) + '.train', 'w') as output:
-        output.write('\n'.join(train))
-        
-    with open((args.input) + '.test', 'w') as output:
-        output.write('\n'.join(test))
-        
-    print('input: %s\nUtterance geneated: %d for training, %d for test' % (args.input, len(train), len(test)))
+    result2 = result2[:args.num_train + args.num_test]
+
+    result3 = []
+    for item in result2:
+        slots = re.findall(slotPattern, item)
+        for slot in slots:
+            words = slot[1]
+            name = slot[2]
+            word = words.split(' ')
+            ret = ' '.join(['{0}/{1}-{2}'.format(word[i], i == 0 and 'B' or 'I', name) for i in range(len(word))])
+            item = item.replace(slot[0], ret)
+        result3.append(item)
+
+    result4 = []
+    for item in result3:
+        data = dict()
+        data['raw'] = item
+        data['uttr'], data['iob'] = extract_iob(item)
+        result4.append(data)
+
+    train = result4[:args.num_train]
+    test = result4[args.num_train:]
+
+    with open(args.input + '.train', 'w') as output:
+        output.write(json.dumps(train))
+
+    with open(args.input + '.test', 'w') as output:
+        output.write(json.dumps(test))
+
+print('%s\nUtterance generated: %d for training, %d for test' % (args.input, len(train), len(test)))
